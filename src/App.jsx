17acc497a959b955707802d3b85b513382a03744
Nigel Maynard <legin8@icloud.com>
2025-02-02 20:44:45 +1300

    feat: added basics of the homepage

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,14 @@ import { Home } from './pages/home';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+    
   )
 }
 
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -7,9 +7,19 @@ export const Home = () => {
             <DNZRDheader />
             <h1>Dunedin Riding for the Disabled</h1>
             <div>
-                <p>At Dunedin RDA we provide therapeutic sessions with horses to improve health and wellbeing outcomes.
-                     for people experiencing disability, or who have specific challenges or needs.</p>
-                <p>We aim to enable and support disabled people to achieve good lives including meaningful participation in, and contribution to, important life activities and roles in their community. We are one of over 40 voluntary (not-for-profit) Riding for Disabled groups around New Zealand, and we are affiliated with the New Zealand Riding for Disabled Association. The Dunedin Group RDA (DRDA) was established in 1973. The location of Dunedin RDA has changed over the years, we now have our own land in Fairfield, Dunedin. Dunedin RDA does not receive government funding and relies on grants, corporate sponsorship and generous donations from the community to keep the the program operating. To find out more about what we offer, check out the pages above. If you have any questions while reading through, email us at office@rdadunedin.org</p>
+                <div className="home-text-blocks">
+                    <p>At Dunedin RDA, we provide therapeutic sessions with horses to help improve health and wellbeing for people with a disability, or who have specific challenges or needs.</p>
+                    <p>We aim to enable and support those experiencing a disability meaningful social, horse and community activities.</p>
+                </div>
+                <div className="home-text-blocks">
+                    <p>We are one of over 40 voluntary (not-for-profit) Riding for Disabled groups around New Zealand.</p>
+                    <p>We are affiliated with the New Zealand Riding for Disabled Association.</p>
+                </div>
+                <div className="home-text-blocks">
+                    <p>The Dunedin RDA was established in 1973.</p>
+                    <p>While the location of Dunedin RDA has changed over the years, we now have our own land in Fairfield, Dunedin.</p>
+                    <p>Dunedin RDA does not receive government funding and relies on grants, corporate sponsorship and generous donations from the community to keep the the program operating.</p>
+                </div>
             </div>
         </>
     )
